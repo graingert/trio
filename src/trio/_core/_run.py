ecd36ffdb459e9063aecd50666c6fbbaa521a9ba
Thomas Grainger <tagrain@gmail.com>
2024-10-26 11:20:15 +0100

    avoid refcycles in trio.run, especially if an Exception is raised

--- src/trio/_core/_run.py
+++ src/trio/_core/_run.py
@@ -1713,6 +1713,7 @@ class Runner:
         self.asyncgens.close()
         if "after_run" in self.instruments:
             self.instruments.call("after_run")
+        self.system_nursery: Nursery | None = None
         # This is where KI protection gets disabled, so we do it last
         self.ki_manager.close()
 
@@ -1927,6 +1928,7 @@ class Runner:
         task._activate_cancel_status(None)
         self.tasks.remove(task)
         if task is self.init_task:
+            self.init_task = None
             # If the init task crashed, then something is very wrong and we
             # let the error propagate. (It'll eventually be wrapped in a
             # TrioInternalError.)
@@ -1937,6 +1939,7 @@ class Runner:
                 raise TrioInternalError
         else:
             if task is self.main_task:
+                self.main_task = None
                 self.main_task_outcome = outcome
                 outcome = Value(None)
             assert task._parent_nursery is not None, task
@@ -2401,12 +2404,15 @@ def run(
         sniffio_library.name = prev_library
     # Inlined copy of runner.main_task_outcome.unwrap() to avoid
     # cluttering every single Trio traceback with an extra frame.
-    if isinstance(runner.main_task_outcome, Value):
-        return cast(RetT, runner.main_task_outcome.value)
-    elif isinstance(runner.main_task_outcome, Error):
-        raise runner.main_task_outcome.error
-    else:  # pragma: no cover
-        raise AssertionError(runner.main_task_outcome)
+    try:
+        if isinstance(runner.main_task_outcome, Value):
+            return cast(RetT, runner.main_task_outcome.value)
+        elif isinstance(runner.main_task_outcome, Error):
+            raise runner.main_task_outcome.error
+        else:  # pragma: no cover
+            raise AssertionError(runner.main_task_outcome)
+    finally:
+        del runner
 
 
 def start_guest_run(
@@ -2815,6 +2821,7 @@ def unrolled_run(
             if isinstance(runner.main_task_outcome, Error):
                 ki.__context__ = runner.main_task_outcome.error
             runner.main_task_outcome = Error(ki)
+        del runner
 
 
 ################################################################
